a4de9c1aa65fa568ac3a81097526b8e70811d02d
FrankXec <choyland35@gmail.com>
2023-04-11 00:30:42 -0600

    Init

---
+++ BarContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DB
+{
+    public class BarContext : DbContext
+    {
+        public BarContext(DbContextOptions<BarContext> options) 
+            : base(options)
+        { 
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Beer> Beers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>().ToTable("Brand");
+            modelBuilder.Entity<Beer>().ToTable("Beer");
+        }
+
+    }
+}
---
+++ Beer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class Beer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BeerId { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public int BrandId { get; set; }
+        [ForeignKey("BrandId")]
+        public virtual Brand? Brand { get; set; }
+    }
+}
